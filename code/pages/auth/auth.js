ed8769478f05cb96a78f69d7d9e15ed0a7b5786e
test <1341159591@qq.com>
2019-06-04 23:34:33 +0800

    fits bugs

--- code/pages/auth/auth.js
+++ code/pages/auth/auth.js
@@ -19,21 +19,47 @@ Page({
     //从缓存中读取用户信息
     wx.getStorage({
       key: 'userInfor',
-      success(res) {
+      success: res => {
         console.log("inStorage:", res.data)
         app.globalData.userInfor = res.data
+        this.getStorageSuccessCallback()
+      },
+      fail: res => {
+        console.log("notInStorage")
       }
     })
     //判断用户是否已注册,如果已经注册直接跳转
+  },
+  toastTimeOut: function () {
+    wx.showToast({
+      title: '网络连接超时',
+      icon: 'none',
+      duration: 2000
+    })
+  }
+  ,
+  onStartTap: function () {
+    wx.showLoading({
+      title: '加载中',
+    })
+    setTimeout(this.toastTimeOut, 30000)
+  },
+  getStorageSuccessCallback: function (){
     const db = wx.cloud.database()
+    console.log(app.globalData.userInfor.openid)
+    console.log("ow8DE5NIfEgGswi5U01M7R6PJQnI" == app.globalData.userInfor.openid)
     db.collection('User').where({
       UserId: app.globalData.userInfor.openid
     }).get({
       success: res => {
         if (res.data.length != 0) {
+          console.log(res.data)
           wx.redirectTo({
             url: '../index/index',
           })
+        }
+        else {
+          console.log('getuserInforFailed')
         }
       }
     })
@@ -52,76 +78,7 @@ Page({
         }
       }
     })
-
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-      if (this.data.hasUserInfo) {
-        wx.redirectTo({
-          url: '../user_create/user_create',
-        })
-        
-      }
-    } else if (this.data.canIUse) {
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-
-        })
-        if (this.data.hasUserInfo) {
-          wx.redirectTo({
-            url: '../user_create/user_create',
-          })
-        }
-      }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          if (res.userInfo) {
-            this.setData({
-              userInfo: res.userInfo,
-              hasUserInfo: true
-            })
-          }
-          if (this.data.hasUserInfo) {
-            wx.redirectTo({
-              url: '../user_create/user_create',
-            })
-          }
-          else {
-            wx.showToast({
-              title: "获取用户信息失败",
-              icon: 'none',
-              duration: 2000
-            })
-          }
-        }
-
-      })
-    }
   },
-  toastTimeOut: function () {
-    wx.showToast({
-      title: '网络连接超时',
-      icon: 'none',
-      duration: 2000
-    })
-  }
-  ,
-  onStartTap: function () {
-    wx.showLoading({
-      title: '加载中',
-    })
-    setTimeout(this.toastTimeOut, 30000)
-  }
-  ,
   getUserInfo: function (e) {
     console.log(e)
     app.globalData.userInfo = e.detail.userInfo
@@ -143,6 +100,7 @@ Page({
         success: res => {
           console.log( res.result.openid)
           app.globalData.userInfor.openid = res.result.openid
+          console.log(res.result)
           //对数据进行缓存
           wx.setStorage({
             key: 'userInfor',
